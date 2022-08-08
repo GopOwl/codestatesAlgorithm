8b4a5b6a979ae02571628c76090fc472e75083fd
GopOwl <viogarden16@gmail.com>
2022-08-08 20:59:51 +0900

    11654번

---
+++ 문자열/b11654/아스키코드.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(__dirname + '/input.txt').toString().trim().split('\n');
+
+solution(input[0]);
+
+function solution(n) {
+  console.log(n.charCodeAt())
+}
