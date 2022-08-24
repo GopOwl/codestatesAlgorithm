0635ad02e5f32bf09bfc0dc676cf4778dbc196ad
GopOwl <viogarden16@gmail.com>
2022-08-24 23:49:47 +0900

    수정

--- 재귀/b10872/팩토리얼.js
+++ 재귀/b10872/팩토리얼.js
@@ -5,8 +5,8 @@ let input = fs.readFileSync(__dirname + '/input.txt').toString().split('\n');
 console.log(solution(+input[0]));
 
 function solution(N) {
-    if(N <= 1) {
-        return 1; 
+    if(N<=1) {
+        return 1;
     }
-    return N*solution(N-1);
+    return N*solution(N-1)
 }
